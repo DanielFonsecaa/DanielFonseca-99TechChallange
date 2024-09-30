75091f5aa75cfbf2530ab449987c6fc605989649
daniel <danieldafonseca@sapo.pt>
2024-09-30 11:54:07 +0100

    problem2 finished

--- src/problem2/problem2-change-currency/src/App.js
+++ src/problem2/problem2-change-currency/src/App.js
@@ -48,7 +48,7 @@ function App() {
   //the price of currency to convert
   const [priceTo, setPriceTo] = useState([]);
   //the amount he will choos to convert from
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState("");
   //the selected currency
   const [token, setToken] = useState("");
   //the selected currency to convert
@@ -63,7 +63,6 @@ function App() {
         "https://interview.switcheo.com/prices.json"
       );
       const data = await response.json();
-      console.log(data);
       setCurrenciesData(data);
       // will take the currency from each object
       const currenciesNames = data.map((item) => item.currency);
@@ -80,13 +79,18 @@ function App() {
     setTimeout(() => {
       //formula to convert is
       // amount to receive = (selected amounted * price of the selected currency) / price of the currency you will receive
-      // amount to receive = amountB    //// amount you put = amountA
-      // price of the selected = priceA //// price of the currency you will receive = priceB
       // so amountB = (amountA * priceA) / priceB
 
       const result = (amount * priceFrom) / priceTo;
-      setMessage(`Conversion Successful! You will receive ${result}`);
-    }, 2000);
+      setMessage(`${amount} ${token} = ${result.toFixed(2)} ${retriveToken}`);
+    }, 1000);
+  };
+
+  const handleSwitchCurrency = (event) => {
+    event.preventDefault();
+    const tempToken = retriveToken;
+    setRetrieveToken(token);
+    setToken(tempToken);
   };
 
   //function that will set the price and the currency you choose
@@ -114,86 +118,98 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Currency converter</h1>
+      <div className="title">
+        <h1 className="">Currency converter</h1>
+        <h5 className="subtitle">Check crypto currency exchange rates</h5>
+      </div>
+
+      <div className="App-header">
         <form className="form" onSubmit={handleSubmit}>
-          <div className="amount-div">
-            <h4 className="amount-title">Amount </h4>
-            <input
-              type="number"
-              value={amount}
-              onChange={(elem) => setAmount(elem.target.value)}
-              placeholder="Choose an Amount"
-              className="amount-input"
-            />
-          </div>
-
-          <div className="from-outer-div">
-            <h4 className="from-title">From</h4>
-            <div className="from-inner-div">
-              <select
-                value={token}
-                onChange={handleFirstTokenAndPrice}
-                className="selectFrom"
-              >
-                <option value="" className="optionFrom">
-                  From
-                </option>
-                {currencies.map((currency) => (
-                  <option key={currency} value={currency} className="">
-                    {currency}
-                  </option>
-                ))}
-              </select>
-              <img src={currencyImg[token]} alt={token} className="imgFrom" />
-            </div>
-
-            {/*<p className="priceFrom">Price: {priceFrom}</p>*/}
-          </div>
-          <button className="change-place">
-            <FontAwesomeIcon
-              icon={faRightLeft}
-              className="fontAwesomeIcon"
-              size="2xl"
-            />
-          </button>
-
-          <div className="to-outer-div">
-            <h4 className="to-title">To</h4>
-            <div className="to-inner-div">
-              <select
-                value={retriveToken}
-                onChange={handleSecondTokenAndPrice}
-                className="selectTo"
-              >
-                <option value="" className="optionTo">
-                  To
-                </option>
-                {currencies.map((currency) => (
-                  <option key={currency} value={currency}>
-                    {currency}
-                  </option>
-                ))}
-              </select>
-              <img
-                src={currencyImg[retriveToken]}
-                alt={retriveToken}
-                className="imgTo"
+          <div className="form-content">
+            <div className="amount-div">
+              <h4 className="amount-title">Amount </h4>
+              <input
+                type="number"
+                value={amount}
+                onChange={(elem) => setAmount(elem.target.value)}
+                placeholder="Choose an Amount"
+                className="amount-input"
               />
             </div>
-            {/*<p>Price: {priceTo}</p>*/}
+
+            <div className="from-outer-div">
+              <h4 className="from-title">From</h4>
+              <div className="from-inner-div">
+                <select
+                  value={token}
+                  onChange={handleFirstTokenAndPrice}
+                  className="selectFrom"
+                >
+                  <option value="" className="optionFrom">
+                    From
+                  </option>
+                  {currencies.map((currency) => (
+                    <option
+                      key={currency}
+                      value={currency}
+                      className="option-from-inside"
+                    >
+                      {currency}
+                    </option>
+                  ))}
+                </select>
+                <img src={currencyImg[token]} alt={token} className="imgFrom" />
+              </div>
+
+              {/*<p className="priceFrom">Price: {priceFrom}</p>*/}
+            </div>
+            <button className="change-place" onClick={handleSwitchCurrency}>
+              <FontAwesomeIcon
+                icon={faRightLeft}
+                className="fontAwesomeIcon"
+                size="2xl"
+              />
+            </button>
+
+            <div className="to-outer-div">
+              <h4 className="to-title">To</h4>
+              <div className="to-inner-div">
+                <select
+                  value={retriveToken}
+                  onChange={handleSecondTokenAndPrice}
+                  className="selectTo"
+                >
+                  <option value="" className="optionTo">
+                    To
+                  </option>
+                  {currencies.map((currency) => (
+                    <option key={currency} value={currency}>
+                      {currency}
+                    </option>
+                  ))}
+                </select>
+                <img
+                  src={currencyImg[retriveToken]}
+                  alt={retriveToken}
+                  className="imgTo"
+                />
+              </div>
+              {/*<p>Price: {priceTo}</p>*/}
+            </div>
           </div>
 
-          <button
-            onClick={handleFetch}
-            disabled={!retriveToken || !token || !amount}
-            className="buttonMakeConversion"
-          >
-            Swap
-          </button>
-          <p>{message}</p>
+          <div className="message-buttonFetch">
+            <p className="message">{message}</p>
+            <button
+              onClick={handleFetch}
+              disabled={!retriveToken || !token || !amount}
+              className="buttonMakeConversion"
+            >
+              Swap
+            </button>
+          </div>
         </form>
-      </header>
+      </div>
       <h2>Hire me please</h2>
     </div>
   );
